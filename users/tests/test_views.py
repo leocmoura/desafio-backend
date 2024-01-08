1fe8690292f930839fc84b2860cd2ca20b3d7e1e
Leonardo <leocordova96@gmail.com>
2024-01-08 16:54:44 -0300

    test: test user registration failure password

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -29,12 +29,48 @@ class UserRegistrationViewTest(APITestCase):
         }
         self.assertEqual(json.loads(response.content), expected_response)
 
-    def test_user_registration_failure(self):
+    def test_user_registration_failure_email(self):
         data = {
             'email': '',
-            'password': 'invalid',
+            'password': 'blank',
+            'password_confirm': 'blank'
+        }
+
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        expected_response = {
+            'email': ['This field may not be blank.']
+        }
+        self.assertEqual(json.loads(response.content), expected_response)
+
+    
+    def test_user_registration_failure_password_small(self):
+        data = {
+            'email': 'testuser@example.com',
+            'password': 'short',
             'password_confirm': 'short'
         }
 
         response = self.client.post(self.url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        expected_response = {
+                'non_field_errors': ["This password is too short. It must contain at least 8 characters."]
+        }
+        self.assertEqual(json.loads(response.content), expected_response)
+        
+    def test_user_registration_failure_password_different(self):
+        data = {
+            'email': 'testuser@example.com',
+            'password': 'invalidpassword',
+            'password_confirm': 'different'
+        }
+
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        expected_response = {
+                'non_field_errors': ["Passwords do not match."]
+        }
+        self.assertEqual(json.loads(response.content), expected_response)
