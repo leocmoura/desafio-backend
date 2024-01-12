a1674e197022af4771f1c13899bed7dae81390e2
Leonardo <leocordova96@gmail.com>
2024-01-12 11:47:22 -0300

    update settings db test deploy render

--- setup/settings.py
+++ setup/settings.py
@@ -81,11 +81,7 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default='postgres://leocmoura:w6IQRo5XUbWM9PKkgFEiEeV9huMRQfhM@dpg-cmgkelgl5elc73fv4m3g-a/filmrankdb',
-        conn_max_age=600
-    )
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 # DATABASES = {
