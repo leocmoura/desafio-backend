e50f93f573d9cd0d960890bbfb28638383cfc774
Leonardo <leocordova96@gmail.com>
2024-01-12 23:13:10 -0300

    build: fix security

--- setup/settings.py
+++ setup/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['film-rank.onrender.com', 'localhost']
 
 
 # Application definition
